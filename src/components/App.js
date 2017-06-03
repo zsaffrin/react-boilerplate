b0944c62bf0a06a043af85d61d4ebce82fb14b9d
Zach Saffrin <zach@zachsaffrin.com>
2017-06-03 14:38:05 -0400

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+require('overeasy/dist/overeasy.min.css');
+
+const App = () => (
+	<Router>
+		<div id="app">
+			<h1>React Boilerplate App</h1>
+		</div>
+	</Router>
+);
+
+export default App;
