9831d6f6ed79e26185897febc32335f9fd4dc7af
Zach Saffrin <zach@zachsaffrin.com>
2017-07-05 16:10:30 -0400

    Include FontAwesome by default

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 require('overeasy/dist/overeasy.min.css');
+require('font-awesome/css/font-awesome.min.css');
 
 const App = () => (
 	<Router>
